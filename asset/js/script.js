8640bf6c8635d3b13c46873760285531ee7d20c3
Hency Basílio <110737595+hencybasilio@users.noreply.github.com>
2024-09-16 13:50:55 +0200

    Add files via upload
    Uploading my landing page files

---
+++ asset/js/script.js
@@ -0,0 +1,14 @@
+/* 
+    Created on : 23/jul/2024, 10:35:04
+    Author     : HENCY BASILIO
+*/
+
+function facebook_button () {
+    alert("Facebook`s LINK not available");
+}
+function linkedin_button () {
+    alert("LINKEDIN`s LINK not available");
+}
+function github_btn () {
+    alert("GITHUB`s LINK not available");
+}
